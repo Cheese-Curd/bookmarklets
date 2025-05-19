c0db7b7468a4b3d8349f1cea3df10efc62b3fdb6
Cheese Curd <51841108+Cheese-Curd@users.noreply.github.com>
2025-05-19 17:34:08 -0400

    Create util.js

---
+++ common/util.js
@@ -0,0 +1,18 @@
+function choice(title = "Choose", ...options)
+{
+	var promptTxt = title;
+	for (var i = 0; i < options.length; i++)
+		promptTxt += "\n " + (i + 1) + ". " + options[i];
+
+	var chosen = prompt(promptTxt)
+	var numb = parseInt(chosen);
+
+	/* Returning the Selection */
+	if (numb === NaN)
+		return -1;
+	else
+		return numb;
+}
+
+const _choice = choice;
+export { _choice as choice };
